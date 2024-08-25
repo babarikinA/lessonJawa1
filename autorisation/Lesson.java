cf8762a117b2474c1a68f71549fe8369c1b3193d
Aliaksandr <aliaksandr@MacBook-Air-Aliaksandr.local>
2024-08-26 06:16:56 +0900

    3

--- autorisation/Lesson.java
+++ autorisation/Lesson.java
@@ -4,35 +4,41 @@ public class Lesson {
 
     public static boolean method(String login, String password, String confirmPassword) {
         if (login.length() <= 20) {
-            return true;
-        } else {
-            try {
-                throw new WrongLoginException();
-            }catch (WrongLoginException e){
-                System.out.println("Неверный логин.");
-            }finally {
-                System.out.println("Попробуйте ввести логин снова!");
-            }
-        }
-        if (password.length() <= 20) {
-            if (password == confirmPassword) {
-                return true;
-            } else {
+            System.out.println("Логин введен верно.");
+            System.out.println("Введите пароль.");
+
+            if (password.length() <= 20) {
+                if (password.equals(confirmPassword)) {
+                    System.out.println("Пароль введен верно.");
+                    System.out.println("Добро пожаловать!");
+                } else {
 
                     try {
 
                         throw new WrongPasswordException();
 
-                    }catch (WrongPasswordException e) {
+                    } catch (WrongPasswordException e) {
                         System.out.println("Неверный пароль.");
 
-                    }finally {
+                    } finally {
                         System.out.println("Попробуйте ввести пароль снова!");
                     }
                 }
             }
-        return false;
+
+        } else {
+            try {
+                throw new WrongLoginException();
+            } catch (WrongLoginException e) {
+                System.out.println("Неверный логин.");
+            } finally {
+                System.out.println("Попробуйте ввести логин снова!");
+            }
+        }
+
+        return true;
+
     }
-    }
+}
 
 
--- autorisation/Test.java
+++ autorisation/Test.java
@@ -3,9 +3,13 @@ package autorisation;
 public class Test {
 
     public static void main(String[] args) {
+
         Lesson.method("alex", "abricos", "abricos");
 
-        System.out.println(Lesson.method("alex","abricos","abricos"));
+        Lesson.method("alex11111111111111111", "abricos", "abricos");
+
+        Lesson.method("alex", "abricos123", "abricos");
+
 
     }
 

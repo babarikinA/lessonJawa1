8c34a67ed7c652787cbfa27b8bc3dff4b6c9257f
Aliaksandr <aliaksandr@MacBook-Air-Aliaksandr.local>
2024-09-03 05:57:33 +0900

    1 push

---
+++ rabotaSfailami/src/Employee.java
@@ -0,0 +1,24 @@
+public class Employee {
+
+        private String firstName;
+        private String lastName;
+        private int age;
+
+        public Employee(String firstName, String lastName, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
---
+++ rabotaSfailami/src/check.java
@@ -0,0 +1,13 @@
+public class check {
+
+    public static void main(String[] args) {
+
+        Tusk1 tusk1 = new Tusk1();
+        tusk1.method1();
+
+        Tusk2 tusk2 = new Tusk2();
+        tusk2.metod1();
+        tusk2.metod2();
+    }
+
+}

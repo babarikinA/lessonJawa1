1a0a1a74941113baa2f69a2aa8f361dd8cc016ec
Aliaksandr <aliaksandr@MacBook-Air-Aliaksandr.local>
2024-09-04 07:25:13 +0900

    1 push

---
+++ RabotaSoStokami/src/Tusk3.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Tusk3 {
+
+    public void metod1() {
+
+        String text = "The Java Runtime Environment (JRE) version 8 is what you get when you download Java software from java.com.";
+
+        String[] array = text.split(" ");
+
+        System.out.println(Arrays.toString(array));
+
+    }
+
+
+}

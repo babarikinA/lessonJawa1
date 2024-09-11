3912c2eba6d06feb0bfe57ef74a44bf32afe5d5c
Aliaksandr <aliaksandr@MacBook-Air-Aliaksandr.local>
2024-09-12 01:04:34 +0900

    1 zadanie

---
+++ Potoki/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Tusk1 tusk1 = new Tusk1();
+        Thread thread = new Thread(tusk1, "potok1");
+        thread.start();
+
+        try {
+            Thread.sleep(2000);
+            //tusk1.off();
+            thread.interrupt();
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Program exit");
+
+    }
+}
---
+++ Potoki/src/Tusk1.java
@@ -0,0 +1,25 @@
+public class Tusk1 implements Runnable{
+
+    private boolean on;
+
+    public void off(){
+        on = false;
+    }
+
+    public Tusk1 () {
+        on = true;
+    }
+
+    //Невозможно сделать так как у нас интерфейс и в нем нету конструктора - выводить в поток имя текущего потока
+    @Override
+    public void run() {
+        System.out.println(String.format("Поток %s работает" , Thread.currentThread().getName()));
+
+        while (Thread.currentThread().isInterrupted() == false){
+            System.out.println("sasha");
+        }
+
+        System.out.println(String.format("Поток %s не работает.", Thread.currentThread().getName()));
+
+    }
+}

e81787b362b690cc55826f3f8587399e08c09f10
aliaksandr <Babarykin.aliaksandr@gmail.com>
2024-10-03 22:42:09 +0900

    2

--- Lymbda/src/Tusk2.java
+++ Lymbda/src/Tusk2.java
@@ -14,7 +14,7 @@ public class Tusk2 {
         heavyBox.offer(new HeavyBox(400));
         heavyBox.offer(new HeavyBox(500));
 
-        IBox removeBox = (HeavyBox) -> HeavyBox.gepoll().getWeight();
+        IBox removeBox = (HeavyBox) -> HeavyBox.getFirst().getWeight();
         System.out.println(removeBox);
 
       //  Consumer<IBox> boxConsumer = iBox -> iBox.removeBox(heavyBox.poll().toString());

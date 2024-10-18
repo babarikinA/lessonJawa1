f963aaf4e53a18d4648c721b5b85461037f1df48
aliaksandr <Babarykin.aliaksandr@gmail.com>
2024-10-18 13:48:05 +0900

    1

---
+++ Selenium/src/test/java/automatization/TestClass.java
@@ -0,0 +1,47 @@
+package automatization;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestClass {
+
+    private static ChromeOptions options;
+    private WebDriver driver;
+
+    @BeforeAll
+    static void downloadDriver() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        options = new ChromeOptions();        // этот объект сохдается только 1 раз
+        options.addArguments("--remote-allow-origins=*");
+    }
+
+    @AfterAll
+    static void exitProgram() {
+
+    }
+
+    @BeforeEach
+    void openBrowser() {
+        driver = new ChromeDriver(options);//на каждый тест новое открытие браузера!бест практис!
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    void closeBrowser() {
+        driver.close();
+    }
+
+    @Test
+    void openBrowserTest() {
+        // MainPage mainPage = new MainPage(driver);
+        //mainPage.openUrl();
+
+        driver.get("https://ru.selenide.org/index.html");
+
+        Assertions.assertEquals("https://ru.selenide.org/index.html", driver.getCurrentUrl());
+
+    }
+}
